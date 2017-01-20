8922254bb33ef250a7dde38b29df055385c2e6ae
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-19 23:03:55 -0600

    filling out app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
 angular
    .module('travelGuides', [ngRoute])
-   .config()
+   .config( function($routeprovider) {
+      $routeprovider
+         .when('/') {
+            controller: "bookCtrl",
+            templatUrl: "/index.html"
+         }
+
+   })
+   .controller('bookCtrl', function($http, $scope) {
+
+         function getBooks() {
+            return $http
+               .get(`data/guides.js`)
+               .then ( (response) => {
+                  console.log(response)
+
+               })
+
+
+         }
+
+   })
